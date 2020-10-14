705661181652794251103ff6da736eb9e15d96ca
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2020-10-14 02:13:55 -0300

    Add guidelines

--- components/src/Guidelines/Guidelines.stories.tsx
+++ components/src/Guidelines/Guidelines.stories.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
 import Guidelines from './index'
 
+export default {
+  title: "Guidelines",
+  component: Guidelines
+} 
 
-
-export const GuidelineExample = () => (
+export const GuidelinesList = () => (
   <Guidelines>
-    <Guidelines.Dos icon= {'✅'} title="DOS">
+    <Guidelines.Dos icon={'✅'} title="Dos">
       <li>Do this!</li>
       <li>Do that!</li>
     </Guidelines.Dos>
-    <Guidelines.Donts icon={'❌'} title="DON'TS">
+    <Guidelines.Donts icon={'❌'} title="Dont's">
       <li>NEVER do this!</li>
       <li>NEVER do that!</li>
     </Guidelines.Donts>
   </Guidelines>
-  
 )
-
-
-
-
-export default {
-  title: "guidelines",
-  component: Guidelines
-}
