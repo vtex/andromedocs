19aa17eb12402959e714b321c5c1e06d4f25d3e4
J. Guilherme Coelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2020-10-16 14:25:46 -0300

    add theme ui plugin and initial config of header

--- andromedocs-theme/gatsby-config.js
+++ andromedocs-theme/gatsby-config.js
@@ -4,9 +4,10 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages/`,
+        path: `${__dirname}/src/documentation/`,
       },
     },
     `gatsby-plugin-mdx`,
+    "gatsby-plugin-theme-ui",
   ],
 };
---
+++ andromedocs-theme/gatsby-node.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === "Mdx") {
+    const value = createFilePath({ node, getNode });
+
+    createNodeField({
+      name: "slug",
+      node,
+      value: `/docs${value}`,
+    });
+  }
+};
+
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query');
+  }
+
+  const posts = result.data.allMdx.edges;
+
+  posts.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/components/layout.tsx`),
+      context: { id: node.id },
+    });
+  });
+};
