e81a3adb8643b1d2652b93254c8fc2d31ddfaed2
J. Guilherme Coelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2020-10-22 16:46:36 -0300

    add a footer to the layout

---
+++ andromedocs-theme/src/components/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Flex } from "theme-ui";
+
+const Footer = () => (
+  <Flex sx={{ height: "183px", backgroundColor: "secondary.base" }}>
+    Footer
+  </Flex>
+);
+
+export default Footer;
