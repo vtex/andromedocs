14331d6250dc37fce6d09e543a8fa3e61f60f72c
J. Guilherme Coelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2020-10-26 17:25:15 -0300

    add a header and footer to the layout

--- andromedocs-theme/src/components/layout/layout.tsx
+++ andromedocs-theme/src/components/layout/layout.tsx
@@ -2,15 +2,17 @@ import React from "react";
 
 import Header from "../header";
 import Footer from "../footer";
-import { Box, Flex, Text} from "theme-ui";
+import { Box, Flex, Text } from "theme-ui";
 
 const Layout = ({ children, title }) => (
-  <Flex sx={{ flexDirection: "column", minHeight: '100vh' }}>
+  <Flex sx={{ flexDirection: "column", minHeight: "100vh" }}>
     <Header />
     <Text as="h1">{title}</Text>
-    <Box as="main" sx={{flex: '1 1 0%'}}> {children}</Box>
+    <Box as="main" sx={{ flex: "1 1 0%" }}>
+      {children}
+    </Box>
     <Footer />
   </Flex>
-)
+);
 
-export default Layout
+export default Layout;
