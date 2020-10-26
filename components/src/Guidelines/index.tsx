c542b0bf6af7e04d1448e350c2cc7cdbf60d3d51
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2020-10-26 15:07:12 -0300

    add corrections

--- components/src/Guidelines/index.tsx
+++ components/src/Guidelines/index.tsx
@@ -13,23 +13,25 @@ const Donts = ({ children, icon = '❌', title = 'Donts' }: Props) => (
   <GuidelinesList icon={icon} title={title} children={children} />
 )
 
-
 const GuidelinesList = ({ children, icon, title }: Props) => {
-
-    return (
-  <Box variant = 'guidelines.list' >
-    <Flex>
-      {icon}
-      <ThemeUIText
-        as="h3"
-        variant = 'guidelines.titles'
-      >
-        {title}
-      </ThemeUIText>
-    </Flex>
-    <ul sx= {{listStyleType: "none", paddingLeft: '28px' }}>{children.map(child => <React.Fragment>{child}<Divider variant ='guidelines.divider'/></React.Fragment> )}</ul>
-  </Box>            
-)
+  return (
+    <Box variant="guidelines.list">
+      <Flex sx={{ alignItems: 'center' }}>
+        {icon}
+        <ThemeUIText as="h3" variant="guidelines.titles">
+          {title}
+        </ThemeUIText>
+      </Flex>
+      <ul sx={{ listStyleType: 'none', paddingLeft: '28px', marginTop: 3 }}>
+        {children.map((child) => (
+          <React.Fragment>
+            {child}
+            <Divider variant="guidelines.divider" />
+          </React.Fragment>
+        ))}
+      </ul>
+    </Box>
+  )
 }
 interface Props {
   children: ReactNode[]
