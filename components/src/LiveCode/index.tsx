04f996db46dc13756eeb20c9f564c4fed8168ab5
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2020-11-10 19:59:26 -0300

    add corrections

--- components/src/LiveCode/index.tsx
+++ components/src/LiveCode/index.tsx
@@ -1,9 +1,12 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import Default from './liveCodeScope'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import Highlight from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/*'
 
-export const LiveCode = ({ scope, code }: Props) => (
-  <LiveProvider scope={scope} code={code}>
+export const LiveCode = ({ scope = {}, code }: Props) => (
+  <LiveProvider scope={{ ...Default, scope }} code={code}>
     <LiveEditor
       sx={{
         bg: 'secondary.base',
